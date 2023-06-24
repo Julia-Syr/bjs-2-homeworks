0f2ce549f7a5aec6adeb99473ee3fad0d74ab272
Julia-Syr <boberia@mail.ru>
2023-06-24 12:40:52 +0300

    Третье задание доработано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,21 +1,11 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
-    return true;
-    } 
-    return false;
+    return (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
     let people = [];
-    let genderFilter;
-    if (gender == "мужской") {
-      genderFilter = users.filter(people => people.gender.includes("мужской"))
-    } else if (gender == "женский") {
-      genderFilter = users.filter(people => people.gender.includes("женский"))
-    } else {
-      return 0;
-    }
-    let arrOfAge = genderFilter.map(people => people.age)
+    let genderFilter = users.filter(people => people.gender === gender);
+    let arrOfAge = genderFilter.map(people => people.age);
     let result = arrOfAge.reduce((acc, item, index) => {
       acc += item;
     if (index === arrOfAge.length - 1) {
