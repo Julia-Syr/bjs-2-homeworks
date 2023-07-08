6e9e100500a9ef2ff0029abe3f9815600f280c41
Julia-Syr <boberia@mail.ru>
2023-07-08 12:36:48 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,63 @@
+class AlarmClock {
+	constructor(alarmCollection, intervalId) {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		} else if (this.alarmCollection.some(element => element.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
+
+		let alarm = {
+			callback: callback,
+			time: time,
+			canCall: true
+		}
+
+		this.alarmCollection.push(alarm);
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(element => element.time !== time)
+	}
+
+	getCurrentFormattedTime() {
+		let currentDate = new Date();
+		let hours = currentDate.getHours().toString();
+		hours = (hours < 10) ? '0' + hours : hours;
+		let minutes = currentDate.getMinutes().toString();
+		minutes = (minutes < 10) ? '0' + minutes : minutes;
+		return `${hours}:${minutes}`;
+	}
+
+	start() {
+		if (!this.intervalId) {
+			this.intervalId = setInterval(() => {
+				let currentTime = this.getCurrentFormattedTime();
+				this.alarmCollection.forEach((alarm) => {
+					if (alarm.time === currentTime && alarm.canCall) {
+						alarm.canCall = false;
+						alarm.callback();
+					}
+				});
+			}, 1000);
+		}
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(alarm => alarm.canCall = true);
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
